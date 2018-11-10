3cb4d838c1608b925037ec568b528310697d380e
Roberto Baquerizo <roba8460@colorado.edu>
2018-11-10 12:28:16 -0700

    modified ring template to build

--- include/ring.h
+++ include/ring.h
@@ -8,10 +8,20 @@
 #ifndef RING_H
 #define RING_H
 
-ring_t *init(int length);
-int insert(ring_t *ring, char data);
-int remove( ring_t *ring, char *data);
-int entries( ring_t *ring);
+typedef struct
+{
+    char *Buffer;
+    int Length;
+    int Ini;
+    int Outi;
+    char full;
+    char empty;
+} ring_t;
+
+int init( ring_t* ring, int length );
+int insert( ring_t *ring, char data );
+int remove( ring_t *ring, char *data );
+int entries( ring_t *ring );
 
 
 
--- source/ring.c
+++ source/ring.c
@@ -6,8 +6,8 @@
 *
 */
 #include "ring.h"
-
-
+#include "stdlib.h"
+/*
 typedef struct
 {
     char *Buffer;
@@ -17,52 +17,83 @@ typedef struct
     char full;
     char empty;
 }ring_t;
+*/
 
-
-int entries( ring_t *ring)
+int entries( ring_t *ring )
 {
-
+	int num = -1;
+	if( 1 == ring->empty )
+	{
+		return num;
+	}
+	num++;
+	while( NULL != ring->Buffer++ )
+	{
+		num++;
+	}
+	return num;
 }
 
-ring_t *init(int length)
+int init( ring_t* rtn, int length )
 {
-    ring_t *rtn = malloc(length+sizeof(*rtn));
-    rtn->Buffer = rtn+1;
-    rtn->Ini = 0;
-    rtn->Outi = 0;
-    rtn->full = 0;
-    rtn->empty = 1;
-    return rtn;
+	int size = length + sizeof( ring_t );
+//    rtn = malloc( length + sizeof( *ring_t ) );
+    rtn = malloc( size );
+	if( NULL != rtn )
+	{
+		rtn->Buffer = (char*)rtn+1;
+		rtn->Ini = 0;
+		rtn->Outi = 0;
+		rtn->full = 0;
+		rtn->empty = 1;
+		rtn->Length = 0;
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
 }
 
 int insert(ring_t *ring, char data)
 {
-    if (!(ring->full))
+    if( !(ring->full) )
     {
         ring->Buffer[ring->Ini] = data;
         ring->Ini++;
-        if (ring->Ini == (ring->Length + ring->Outi))ring->full = 1;
-    }
-    else return -1;
-}
-int remove( ring_t *ring, char *data)
-{
-    if (!(ring->empty))
-    {
-        *data = ring->Buffer[ring->Outi];
-        ring->Outi++;
-        ring->full = 0;
+		ring->Length++;
+        if( ring->Ini == ( ring->Length + ring->Outi ) )
+		{
+			ring->full = 1;
+		}
+		return 1;
     }
     else return -1;
 }
 
-test1(void)
+int remove( ring_t *ring, char *data)
 {
-    ring_t my_ring, *my_ringp = &my_ring;
+    if( !(ring->empty) )
+    {
+        *data = ring->Buffer[ring->Outi];
+        ring->Outi++;
+        ring->full = 0;
+		return 1;
+    }
+    else return -1;
+}
+
+#ifdef UNIT_TEST
+void test1(void)
+{
+    ring_t my_ring;
+	ring_t* my_ringp = &my_ring;
     init(my_ringp, 100);
     for(int i = 0; i < 100; i ++)
     {
         assert(insert(my_ringp, 'a')== 0);
     }
     assert(insert(my_ringp, 'a')== -1);
+	return void;
 }
+#endif
