ef9999d4a3757b92199caa31f09530c18530387f
Roberto Baquerizo <roba8460@colorado.edu>
2018-11-11 23:49:47 -0700

    implemented tests for resizing buffer and getting number of elements

--- Unittest/include/unittest.h
+++ Unittest/include/unittest.h
@@ -8,6 +8,11 @@
 #ifndef TEST_H
 #define TEST_H
 
-void test2(void);
+char DATA[100];
+
 void test1(void);
+void test2(void);
+void test3(void);
+void test4(void);
+void test5(void);
 #endif
--- Unittest/source/unittest.c
+++ Unittest/source/unittest.c
@@ -18,15 +18,16 @@
 //can be written to after full
 void test1(void)
 {
-  ring_t my_ring;
-  ring_t* my_ringp = &my_ring;
-  my_ringp = init(100);
-  for(int i = 0; i < 100; i ++)
-  {
-      CU_ASSERT(insert(my_ringp, 'a')== 1);
-  }
-  CU_ASSERT(insert(my_ringp, 'a')== -1);
+    ring_t my_ring;
+    ring_t* my_ringp = &my_ring;
+    my_ringp = init(100);
+    for(int i = 0; i < 100; i ++)
+    {
+        CU_ASSERT(insert(my_ringp, 'a') == 1);
+    }
+    CU_ASSERT(insert(my_ringp, 'a') == -1);
 }
+
 //Test Description: Remove data from buffer, ensure that remove will not
 //execute after buffer is empty
 void test2(void)
@@ -46,5 +47,86 @@ void test2(void)
   }
   CU_ASSERT(removeData(my_ringp2, DATA)== -1);
 }
-//Test Description: Fill buffer with data and test to see if buffer
-//can be written to after full
+
+//Test Description: Fill buffer and then attempt to increase size.
+void test3(void)
+{
+  ring_t my_ring3;
+  ring_t* my_ringp3 = &my_ring3;
+  my_ringp3 = init(100);
+
+  for(int i = 0; i < 100; i ++)
+  {
+    printf("filling up iter %u\n\r", i);
+      CU_ASSERT(insert(my_ringp3, 'a') == 1 );
+  }
+  // Add another element - this should fail
+  CU_ASSERT(insert(my_ringp3, 'a') == -1);
+
+  CU_ASSERT(resize(my_ringp3, 110) == 1);
+
+  printf("Length of buffer after resize is %u\n\r", my_ringp3->Length);
+  printf("After resize should not be full %u\n\r", my_ringp3->full);
+
+  printf("Outi = %u and Ini = %u\n\r", my_ringp3->Outi, my_ringp3->Ini);
+  // Attempt to add additional elements
+  for( int i = 0; i < 10; i++)
+  {
+    printf("Iteration after resize %u\n\r", i);
+    CU_ASSERT(insert(my_ringp3, 'a') == 1);
+  }
+
+  // One more element should fail again
+  CU_ASSERT(insert(my_ringp3, 'a') == -1);
+}
+
+//Test Description: Test shrinking the size of the buffer
+void test4(void)
+{
+  ring_t my_ring4;
+  ring_t* my_ringp4 = &my_ring4;
+  my_ringp4 = init(100);
+
+  for(int i = 0; i < 100; i ++)
+  {
+      printf("filling up iter %u\n\r", i);
+      CU_ASSERT(insert(my_ringp4, 'a') == 1 );
+  }
+
+  // Add another element - this should fail
+  CU_ASSERT(insert(my_ringp4, 'a') == -1);
+
+  // Attempt to shrink after buffer is full
+  CU_ASSERT(resize(my_ringp4, 90) == -2);
+
+  printf("Length of buffer after resize is %u\n\r", my_ringp4->Length);
+
+  printf("Outi = %u and Ini = %u\n\r", my_ringp4->Outi, my_ringp4->Ini);
+
+  // Remove ten items
+  for( int i = 0; i < 10; i++)
+  {
+    printf("Iteration after resize %u\n\r", i);
+    CU_ASSERT(removeData(my_ringp4, DATA) == 1);
+  }
+
+  printf("After removing items, buffer should not be full %u\n\r", my_ringp4->full);
+  // Attempt shrink again
+  CU_ASSERT(resize(my_ringp4, 90) == 1);
+}
+
+void test5(void)
+{
+  ring_t my_ring5;
+  ring_t* my_ringp5 = &my_ring5;
+  my_ringp5 = init(100);
+
+  // Add 15 elements
+  for( int i  = 0; i < 15; i++)
+  {
+    CU_ASSERT(insert(my_ringp5, 'a') == 1);
+    CU_ASSERT(entries(my_ringp5) == i+1);
+  }
+  CU_ASSERT(entries(my_ringp5) == 15);
+}
+
