07b17341841e5fa8b5f02e068af2cd163adbe411
Roberto Baquerizo <roba8460@colorado.edu>
2018-11-11 23:50:03 -0700

    added new tests to test suite

--- source/main.c
+++ source/main.c
@@ -4,21 +4,21 @@
 #include <CUnit/Basic.h>
 #define UNIT_TEST
 
-char* DATA[1000];
+extern char DATA[100];
 
 int main(void)
 {
-    //char* data;
     CU_initialize_registry();
-    CU_pSuite suite = CU_add_suite("test1_test_suite", 0, 0);
+    CU_pSuite suite = CU_add_suite("test_suite", 0, 0);
 
     CU_add_test(suite, "test_add", test1);
     CU_add_test(suite, "test_remove", test2);
-    
+    CU_add_test(suite, "test_grow", test3);
+    CU_add_test(suite, "test_shrink", test4);
+    CU_add_test(suite, "test_elements", test5);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
-  //test1();
     return 0;
 }
